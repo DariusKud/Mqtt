bda483dc3c9c2f00a484f3237782128c280c54eb
DariusKud <dariuslt23@gmail.com>
2020-01-20 19:03:36 +0000

    Added temperature.txt and MqttServer.cpp files. Temperature reading from file works.

--- MqttProducer.cpp
+++ MqttProducer.cpp
@@ -1,1 +1,47 @@
-Test test test
+#include<iostream>
+#include<fstream>
+#include<string>
+
+
+void ReadTemperatureFromFile();
+
+float temperature;
+
+using namespace std;
+
+int main()
+{
+	cout<<"Calling file read procedure"<<endl;
+	ReadTemperatureFromFile();
+	return 0;
+}
+
+void ReadTemperatureFromFile() {
+
+
+	const string temperatureString = "t=";
+	cout<<"Reading file..."<<endl;
+	ifstream file ("temperature.txt");
+
+	if(!file.is_open())
+	{
+		cerr << "Failed to open file!" << endl;
+		
+	}
+
+	string temperatureLine;
+	size_t position;
+	while (getline(file, temperatureLine))
+	{
+		if(temperatureLine.find(temperatureString) != string::npos)
+		{
+			cout << "Temperature Line at" << endl;
+			cout << temperatureLine << endl;
+			position = temperatureLine.find(temperatureString);
+			cout << "Temperature index = "<< position << endl;
+			temperature = stof(temperatureLine.substr(position + 2))/1000;
+			cout <<"Temperature = " << temperature <<endl;
+		}
+	}
+
+}
